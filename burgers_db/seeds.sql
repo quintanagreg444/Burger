429405a5ce7b3901f0b90db9b140d77c93515245
Greg Quintana <quintana.greg444@gmail.com>
2019-09-18 21:14:47 -0600

    initial commit

---
+++ burgers_db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES('Double CheeseBurger');
